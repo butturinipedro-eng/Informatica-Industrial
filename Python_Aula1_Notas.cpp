8d2fdb949411d184a4887ede7ad6ddf9c8c3c589
butturinipedro-eng <butturini.pedro@estudante.ufjf.br>
2025-10-23 16:49:07 -0300

    Create Python_Aula1_Notas.cpp

---
+++ Python_Aula1_Notas.cpp
@@ -0,0 +1,15 @@
+// python fracamente tipada
+// posso fazer
+// a = 10
+// a = "ENE118" 
+// versao x.y.z; se mudar x e y provavelmente nao funcionará, se mudar z muda
+// l = [1,2,3,4] iteravel
+// N = 10 não iteravel
+// S = "ENE118" iterável
+// for x in l:
+//   print(x)
+// for c in s:
+//   print(c)
+// for i in range(2,20,2) vai de 2 a 20 de 2 em 2
+//   print(i)
+ 
