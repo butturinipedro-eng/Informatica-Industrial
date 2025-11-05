84512f509c8a31ef32071a16efdd0539aa4f72e7
butturinipedro-eng <butturini.pedro@estudante.ufjf.br>
2025-11-05 16:31:42 -0300

    Update Notas.py

--- Aula 15/Notas.py
+++ Aula 15/Notas.py
@@ -11,3 +11,11 @@ Accept: Comando bloqueante -> bloqueia o fluxo da execução do programa, só co
 Servidor não precisa saber o IP e a porta do cliente, mas o cliente precisa saber para passar essas informações para o connect.
 Feito isso é criado um tunel de comunicaão entre servidor e cliente. Agora a comunicação é tão simples quanto escrever em um arquvio
 Cliente escreve primeiro pois faz requerimentos ao servidor e o servidor responde e assim sucessivamente.
+No write escrevo bits, toda a comunicaão é feita por bits
+Protocolo é a forma como os bits são organizados.
+
+----Modulo Socket----
+Métodos chamados bind, socket, write, listen etc. Métodos que invocam as primitivas do socket no sistema operacional
+
+---Exceções---
+Problemas não previstos durante a construção do código
