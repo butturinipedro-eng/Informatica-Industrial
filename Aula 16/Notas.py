784ccb40579d69ffc26c6784c1332d979c5a1b34
butturinipedro-eng <butturini.pedro@estudante.ufjf.br>
2025-11-07 17:41:57 -0300

    Add implementation flow for generic service in Notas.py

---
+++ Aula 16/Notas.py
@@ -0,0 +1,7 @@
+"""
+---Fluxo de implementação de um serviço genérico---
+ Passo 1: Ler mensagens em bytes
+ Passo 2: Decodificar msg com base no portocolo da camada de aplicação
+ Passo 3: Processar requisição
+ Passo 4: Codifico a resposta
+ Passo 5: Enviar mensagem codificada
